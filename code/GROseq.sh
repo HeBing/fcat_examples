9debfbaa2b71c4c985bfdade1142639aa3c6bf0f
HeBing <bhe3@jhu.edu>
2016-05-24 21:05:43 -0400

    more info

--- code/GROseq.sh
+++ code/GROseq.sh
@@ -1,13 +1,13 @@
 # Download ATAC-seq bam files
 
 # Count coverage
-./countCoverage -i ./bamFiles_GROseq -p ./param_GROseq_short
+./countCoverage -i ./bamFiles_GROseq -p ./param_GROseq
 
 # Extract features for testing
-./extractFeature -i ./coveragesFile_GROseq -t ../testingDat/CMYCData_narrowPeak_Gm12878_genomewide.txt -o ./Feature_GROseq_CMYCData_narrowPeak_Gm12878_genomewide.txt -p ./param_GROseq
+./extractFeature -i ./coveragesFile_GROseq -t ../testingDat/testingRegion_GROseq_Gm12878.txt -o ./Feature_testingRegion_GROseq_Gm12878.txt -p ./param_GROseq
 
 # Extract features for training
-./extractFeature -i ./coveragesFile_GROseq -t ../trainingDat/mixedTraining.txt -o ./Feature_GROseq_mixedTraining.txt -p ./param_GROseq
+./extractFeature -i ./coveragesFile_GROseq -t ../trainingDat/trainingRegion_GROseq_Gm12878.txt -o ./Feature_trainingRegion_GROseq_Gm12878.txt -p ./param_GROseq
 
 # fcat
-python fcat.py -model LogisticRegressionL1,LogisticRegressionL2,RandomForest -train ./Feature_GROseq_mixedTraining.txt -test ./Feature_GROseq_CMYCData_narrowPeak_Gm12878_genomewide.txt -output default
+python fcat.py -model LogisticRegressionL1,LogisticRegressionL2,RandomForest -train ./Feature_trainingRegion_GROseq_Gm12878.txt -test ./Feature_testingRegion_GROseq_Gm12878.txt -output default
